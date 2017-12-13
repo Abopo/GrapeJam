d0f76531c72fb20438e1da328a3b490c1643df75
Josh Dupuis <dupuisjosh@gmail.com>
2017-12-12 22:53:04 -0500

    Changeed Gitignore Again

--- Grape Jam/Assets/Scripts/Visuals/Billboard.cs
+++ Grape Jam/Assets/Scripts/Visuals/Billboard.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -17,23 +16,3 @@ public class Billboard : MonoBehaviour {
         transform.LookAt(depthCamera.transform.position, depthCamera.transform.up);
 	}
 }
-=======
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Billboard : MonoBehaviour {
-
-    public Camera depthCamera;
-
-	// Use this for initialization
-	void Start () {
-        depthCamera = GameObject.FindGameObjectWithTag("Depth Camera").GetComponent<Camera>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        transform.LookAt(depthCamera.transform.position, depthCamera.transform.up);
-	}
-}
->>>>>>> 95f42c383b876b69eedb3c9aacbbaf019b12822f
